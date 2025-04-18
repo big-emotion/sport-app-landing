705286f66815fbd4ab6110a87e6ad966c0ab7ec7
jnk <kollojeannoe@gmail.com>
2025-04-18 09:52:35 +0200

    feat: remove clsx

--- src/app/components/ui/Section.tsx
+++ src/app/components/ui/Section.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { ReactNode } from 'react'
-import clsx from 'clsx'
 
 interface SectionProps {
   children: ReactNode
@@ -12,11 +11,8 @@ interface SectionProps {
 
 export function Section({ children, className, id, fullWidth = false }: SectionProps) {
   return (
-    <section id={id} className={clsx('py-16 md:py-24', className)}>
-      <div className={clsx(
-        'mx-auto px-4 sm:px-6 lg:px-8',
-        !fullWidth && 'max-w-7xl'
-      )}>
+    <section id={id} className={`py-16 md:py-24 ${className || ''}`}>
+      <div className={`mx-auto px-4 sm:px-6 lg:px-8 [&:not(.full-width)]:max-w-7xl ${fullWidth ? 'full-width' : ''}`}>
         {children}
       </div>
     </section>
