8050f97f1202b4b5b49dccc89414de801eb62f95
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-04-19 18:03:06 +0200

    fix: fix responsive design

--- src/app/components/ui/FeatureCard.tsx
+++ src/app/components/ui/FeatureCard.tsx
@@ -9,7 +9,7 @@ export function FeatureCard({ titleKey, subtitleKey }: FeatureCardProps) {
     const t = useTranslations('features');
 
     return (
-        <div className="w-[288px] sm:w-[300px] bg-white rounded-3xl text-center flex items-center flex-col
+        <div className="w-[288px] sm:w-[250px] md:w-[300px] bg-white rounded-3xl text-center flex items-center flex-col
         justify-center gap-3 py-12">
             <div className="bg-yellow-400 w-20 h-20">img</div>
             <h3 className="font-bold text-2xl text-black">{t(titleKey)}</h3>
