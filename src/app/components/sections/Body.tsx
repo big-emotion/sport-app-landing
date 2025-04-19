92c36711f0a6945864aad6f0bb5f80ba5928d401
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-04-19 13:55:03 +0200

    feat: add animation for feature section

--- src/app/components/sections/Body.tsx
+++ src/app/components/sections/Body.tsx
@@ -1,21 +1,64 @@
-import {useTranslations} from "next-intl";
-import {FeatureCard} from "@/src/app/components/ui/FeatureCard";
+'use client'
+
+import { useTranslations } from "next-intl";
+import { FeatureCard } from "@/src/app/components/ui/FeatureCard";
+import { motion } from "framer-motion";
+
+const container = {
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren: 0.3,
+        },
+    },
+};
+
+const item = {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 0 },
+};
 
 export function Body() {
     const t = useTranslations('features');
 
     return (
-        <div className="flex flex-col bg-amber-50 h-[10000px] m-8 sm:m-10 lg:m-20">
-            <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-center mb-12 text-black">{t('title')}</h2>
+        <div className="flex flex-col bg-amber-50 h-auto m-8 sm:m-10 lg:m-20 h-[10000px]" >
+            <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-center mb-12 text-black">
+                {t('title')}
+            </h2>
+
             <div className="flex flex-col gap-16">
-                <div className="flex flex-col sm:flex-row justify-center items-center sm:gap-16 gap-8">
-                    <FeatureCard titleKey="localisation" subtitleKey="subtitle_localisation" />
-                    <FeatureCard titleKey="sport" subtitleKey="subtitle_sport" />
-                </div>
-                <div className="flex flex-col sm:flex-row justify-center items-center sm:gap-16 gap-8">
-                    <FeatureCard titleKey="share" subtitleKey="subtitle_share" />
-                    <FeatureCard titleKey="social" subtitleKey="subtitle_social" />
-                </div>
+                {/* Première ligne de cartes */}
+                <motion.div
+                    className="flex flex-col sm:flex-row justify-center items-center sm:gap-16 gap-8"
+                    variants={container}
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: true, amount: 0.3 }}
+                >
+                    <motion.div variants={item} >
+                        <FeatureCard titleKey="localisation" subtitleKey="subtitle_localisation" />
+                    </motion.div>
+                    <motion.div variants={item} >
+                        <FeatureCard titleKey="sport" subtitleKey="subtitle_sport" />
+                    </motion.div>
+                </motion.div>
+
+                {/* Deuxième ligne de cartes */}
+                <motion.div
+                    className="flex flex-col sm:flex-row justify-center items-center sm:gap-16 gap-8"
+                    variants={container}
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: true, amount: 0.3 }}
+                >
+                    <motion.div variants={item} >
+                        <FeatureCard titleKey="share" subtitleKey="subtitle_share" />
+                    </motion.div>
+                    <motion.div variants={item} >
+                        <FeatureCard titleKey="social" subtitleKey="subtitle_social" />
+                    </motion.div>
+                </motion.div>
             </div>
         </div>
     );
