2da2c189db2952c050e5a2c1520ef6f28be28b58
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-04-24 14:17:35 +0200

    reset hard from dev, rebase & cherrypick

--- src/app/components/sections/Advantages.tsx
+++ src/app/components/sections/Advantages.tsx
@@ -1,68 +1,80 @@
-"use client";
+'use client';
 
-import { useTranslations } from "next-intl";
-import { AccessItem } from "@/src/app/components/ui/AccessItem";
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
+import { useTranslations } from 'next-intl';
+import { JSX } from 'react';
 
-export default function Advantages() {
-    const t = useTranslations('advantage');
+import { AccessItem } from '@/app/components/ui/AccessItem';
 
-    return (
-        <div>
-            <motion.h2
-                className="text-4xl sm:text-5xl lg:text-6xl font-bold text-center mb-12 text-black mt-16"
-                initial={{ opacity: 0, y: -50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8 }}
-            >
-                {t("title")}
-            </motion.h2>
+export default function Advantages(): JSX.Element {
+  const t = useTranslations('advantage');
 
-            <div className="flex flex-col gap-10 items-center">
-                {/* Ligne 1 */}
-                <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-                    <motion.div
-                        className="flex"
-                        initial={{ opacity: 0, x: -50 }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.8, delay: 0.2 }}
-                        viewport={{ once: true, amount: 0.3 }}
-                    >
-                        <AccessItem title={t("access_title")} subtitle={t("access_subtitle")} />
-                    </motion.div>
-                    <motion.div
-                        className="flex"
-                        initial={{ opacity: 0, x: 50 }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.8, delay: 0.4 }}
-                        viewport={{ once: true, amount: 0.3 }}
-                    >
-                        <AccessItem title={t("commu_title")} subtitle={t("commu_subtitle")} />
-                    </motion.div>
-                </div>
+  return (
+    <div>
+      <motion.h2
+        className="text-4xl sm:text-5xl lg:text-6xl font-bold text-center mb-12 text-black mt-16"
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        {t('title')}
+      </motion.h2>
 
-                {/* Ligne 2 */}
-                <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-                    <motion.div
-                        className="flex"
-                        initial={{ opacity: 0, x: -50 }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.8, delay: 0.6 }}
-                        viewport={{ once: true, amount: 0.3 }}
-                    >
-                        <AccessItem title={t("discovery_title")} subtitle={t("discovery_subtitle")} />
-                    </motion.div>
-                    <motion.div
-                        className="flex"
-                        initial={{ opacity: 0, x: 50 }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.8, delay: 0.8 }}
-                        viewport={{ once: true, amount: 0.3 }}
-                    >
-                        <AccessItem title={t("multisport_title")} subtitle={t("multisport_subtitle")} />
-                    </motion.div>
-                </div>
-            </div>
+      <div className="flex flex-col gap-10 items-center">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10">
+          <motion.div
+            className="flex"
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+            viewport={{ once: true, amount: 0.3 }}
+          >
+            <AccessItem
+              title={t('access_title')}
+              subtitle={t('access_subtitle')}
+            />
+          </motion.div>
+          <motion.div
+            className="flex"
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.4 }}
+            viewport={{ once: true, amount: 0.3 }}
+          >
+            <AccessItem
+              title={t('commu_title')}
+              subtitle={t('commu_subtitle')}
+            />
+          </motion.div>
         </div>
-    );
+
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10">
+          <motion.div
+            className="flex"
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.6 }}
+            viewport={{ once: true, amount: 0.3 }}
+          >
+            <AccessItem
+              title={t('discovery_title')}
+              subtitle={t('discovery_subtitle')}
+            />
+          </motion.div>
+          <motion.div
+            className="flex"
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.8 }}
+            viewport={{ once: true, amount: 0.3 }}
+          >
+            <AccessItem
+              title={t('multisport_title')}
+              subtitle={t('multisport_subtitle')}
+            />
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/components/ui/AccessItem.tsx
+++ src/app/components/ui/AccessItem.tsx
@@ -1,20 +1,22 @@
 'use client';
 
+import { JSX } from 'react';
+
 interface AccessItemProps {
-    title: string;
-    subtitle: string;
+  title: string;
+  subtitle: string;
 }
 
-export function AccessItem({ title, subtitle }: AccessItemProps) {
-    return (
-        <div className="w-[325px] p-4">
-            <div className="flex">
-                <div className="w-12 h-12 aspect-square bg-yellow-400 rounded-full mr-[24px]"></div>
-                <div>
-                    <h3 className="text-lg font-bold text-black ">{title}</h3>
-                    <p className="text-gray-700">{subtitle}</p>
-                </div>
-            </div>
+export function AccessItem({ title, subtitle }: AccessItemProps): JSX.Element {
+  return (
+    <div className="w-[325px] p-4">
+      <div className="flex">
+        <div className="w-12 h-12 aspect-square bg-yellow-400 rounded-full mr-[24px]"></div>
+        <div>
+          <h3 className="text-lg font-bold text-black ">{title}</h3>
+          <p className="text-gray-700">{subtitle}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
