4c0f61a4a32b5c62cd9aebdfb740c39164f7455e
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-04-26 21:42:49 +0200

    fix: fix aniamtion for section FAQ

--- src/app/components/sections/Faq.tsx
+++ src/app/components/sections/Faq.tsx
@@ -73,10 +73,10 @@ export default function Faq(): JSX.Element {
                 {openIndex === index && (
                   <motion.div
                     key="content"
-                    initial={{ height: 0, opacity: 0 }}
-                    animate={{ height: 'auto', opacity: 1 }}
-                    exit={{ height: 0, opacity: 0 }}
-                    transition={{ duration: 0.5 }}
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 20 }}
+                    transition={{ duration: 0.4 }}
                     className="px-6 py-4 text-black overflow-hidden"
                   >
                     <div>{t(`answers.${item.answerKey}`)}</div>
