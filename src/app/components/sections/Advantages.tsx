c382de6693aa1a050edf70d21462fdae78b333cd
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-04-27 00:11:53 +0200

    fix: fix animation

--- src/app/components/sections/Advantages.tsx
+++ src/app/components/sections/Advantages.tsx
@@ -16,6 +16,7 @@ export default function Advantages(): JSX.Element {
         initial={{ opacity: 0, y: -50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
+        viewport={{ once: true, amount: 0.3 }}
       >
         {t('title')}
       </motion.h2>
