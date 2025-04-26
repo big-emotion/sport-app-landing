2a4760ee08943d029e073c60d0124fc0472944eb
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-04-27 00:03:24 +0200

    fix: fix page.tsx for add advantages section for vercel preview

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 import { Header } from '@/app/components/sections/Header';
+import Advantages from '@/app/components/sections/Advantages';
 
 export default async function Home(): Promise<React.ReactNode> {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Advantages />
+    </div>
+  );
 }
