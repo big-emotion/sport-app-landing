257eeb2d5f4831d1426c965fec38fe05b5340836
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-04-27 16:00:13 +0200

    feat: add faq section to page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Faq from '@/app/components/sections/Faq';
 import { Header } from '@/app/components/sections/Header';
 
 export default async function Home(): Promise<React.ReactNode> {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Faq />
+    </div>
+  );
 }
