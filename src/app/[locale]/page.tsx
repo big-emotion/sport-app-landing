5830aad0ff64826a24cae2a92c2862df6bd9f43a
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-04-27 15:57:12 +0200

    feat: add Newsletter section to page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 import { Header } from '@/app/components/sections/Header';
+import Newsletter from '@/app/components/sections/Newsletter';
 
 export default async function Home(): Promise<React.ReactNode> {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Newsletter />
+    </div>
+  );
 }
