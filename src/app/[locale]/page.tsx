b9e771c06df83284755145995d24e8d40242ef4e
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-04-27 15:51:26 +0200

    feat: add Galerie section to page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 import { Header } from '@/app/components/sections/Header';
+import { Galerie } from '@/app/components/sections/Galerie';
 
 export default async function Home(): Promise<React.ReactNode> {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Galerie />
+    </div>
+  );
 }
